418cc1fd49fba79b68028da306abd08b9ade1d87
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-01-27 16:02:19 +0530

    Initial COmmit 2

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ dotenv.config({ path: './config/config.env'})
 
 const app = express()
 
+app.get('/api/v1/bootcamps',(req,res) => {
+    res.status(200).json({ success : true , msg : 'Show all bootcamps'})
+})
+app.post('/api/v1/bootcamps',(req,res) => {
+    res.status(200).json({ success : true , msg : 'Create new Bootcamp'})
+})
+app.put('/api/v1/bootcamps/:id',(req,res) => {
+    res.status(200).json({ success : true , msg : `Update the bootcamp with id ${req.params.id}`})
+})
+app.get('/api/v1/bootcamps/:id',(req,res) => {
+    res.status(200).json({ success : true , msg : `Get Bootcamp with id ${req.params.id}`})
+})
+app.delete('/api/v1/bootcamps/:id',(req,res) => {
+    res.status(200).json({ success : true , msg : `Delete the bootcamp with id ${req.params.id}`})
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT ,console.log(`Server running in ${process.env.NODE_ENV} mode and on ${process.env.PORT} port`))
