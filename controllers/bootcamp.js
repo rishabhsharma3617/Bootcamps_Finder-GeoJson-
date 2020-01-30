37011b249261e75c6faefaed388310c45c0fcf3f
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-01-30 19:46:46 +0530

    Added the pagination variable

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -43,14 +43,32 @@ exports.getBootcamps =asyncHandler( async (req,res,next) => {
          const page = parseInt(req.query.page, 10) || 1
          const limit = parseInt(req.query.limit, 10) || 1
          const skip = (page - 1) * limit
-
+         const startIndex = (page - 1) * limit
+         const endIndex = page * limit
+         const total = await Bootcamp.countDocuments()
 
          query = query.skip(skip).limit(limit)   
 
 
         //Executing Query
         const bootcamps = await query
-        res.status(200).json({ success : true ,data : bootcamps})
+
+        //Pagination result 
+        const pagination = {}
+
+        if(endIndex < total){
+            pagination.next = {
+                page : page + 1,
+                limit
+            }
+        }
+        if(startIndex > 0) {
+            pagination.prev = { 
+                page : page -1,
+                limit
+            }
+        }
+        res.status(200).json({ success : true ,pagination : pagination ,data : bootcamps})
 
     
 })
