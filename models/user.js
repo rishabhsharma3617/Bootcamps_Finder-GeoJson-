f76a30559c05ed2c1042752ffbb10b3a782a541b
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-01-31 20:29:40 +0530

    Implemented the bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const UserSchema = new mongoose.Schema({
     name : {
         type: String,
@@ -35,5 +36,10 @@ UserSchema.pre('save', async function(next) {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password , salt)
 })
+
+// SIgn JWt and return
+UserSchema.methods.getSignedJwtToken = function() {
+    return jwt.sign({ id : this._id})
+}
 
 module.exports = mongoose.model('User',UserSchema)
