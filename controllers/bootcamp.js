864cfd8fc19151077d6ff1a40448e7e1b497784a
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-02-01 20:05:26 +0530

    Added the Bootcamp ownership

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -55,14 +55,23 @@ exports.createBootcamp = asyncHandler( async (req,res,next) => {
 // @access          Private
 exports.updateBootcamp = asyncHandler( async (req,res,next) => {
 
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id , req.body ,{
+        let bootcamp = await Bootcamp.findById(req.params.id)
+        if(!bootcamp)
+        {
+            return next(new ErrorResponse(`Bootcamp not found with the  ${req.params.id}`,404))
+        }
+
+        //Make Usure User is bootcamp owner
+        if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return next(
+                new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`,401)
+            )
+        }
+        bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body ,{
             new : true,
             runValidators : true
         })
-        if(!bootcamp)
-        {
-            return next(new ErrorResponse(`Bootcamp not found with the error respons ${req.params.id}`,404))
-        }
+
         res.status(200).json({ success : true , msg : `Update the bootcamp with id ${req.params.id}`})
 })
 
@@ -76,6 +85,12 @@ exports.deleteBootcamp = asyncHandler( async (req,res,next) => {
         if(!bootcamp)
         {
             return next(new ErrorResponse(`Bootcamp not found with the error respons ${req.params.id}`,404))
+        }
+         //Make Usure User is bootcamp owner
+         if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return next(
+                new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`,401)
+            )
         }
         bootcamp.remove()
         res.status(200).json({ success : true ,  data : null})
@@ -150,6 +165,13 @@ exports.bootcampPhotoUpload = asyncHandler( async (req,res,next) => {
     
     if(!req.files) {
         return next(new ErrorResponse(`Please upload a file`,400))
+    }
+
+     //Make Usure User is bootcamp owner
+     if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`,401)
+        )
     }
     
     const file = req.files.file
